cffb87df1030b652b7b883a8af017126161ff8dc
Hemant-Makar <hemant.makar@gmail.com>
2021-05-14 14:23:56 +0530

    1 file modified

--- src/app/ngrx-store/actions.ts
+++ src/app/ngrx-store/actions.ts
@@ -1,1 +1,7 @@
 import { createAction, props } from '@ngrx/store';
+import { IMonitorData } from '../models/imonitor-data';
+
+export const addWidgetData = createAction(
+  '[Data] add widget data',
+  props<{ data: IMonitorData }>()
+);
