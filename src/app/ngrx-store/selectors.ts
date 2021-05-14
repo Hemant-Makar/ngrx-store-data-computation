2045348f9ad283f45a85b0a1fc74268e4ac22b47
Hemant-Makar <hemant.makar@gmail.com>
2021-05-14 16:01:09 +0530

    1 file modified

--- src/app/ngrx-store/selectors.ts
+++ src/app/ngrx-store/selectors.ts
@@ -2,13 +2,19 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { IState } from './istate';
 export const selectFeatureState = createFeatureSelector<IState>('liveData');
 
-export const getWidgettData = createSelector(
+export const widgetFeature = createSelector(
   selectFeatureState,
   (state: IState) => {
     return state.data;
   }
 );
 
+export const getWidgetData = createSelector(
+  widgetFeature,
+  (widget, id: string) => {
+    return widget[id];
+  }
+);
 export const getCount = createSelector(
   selectFeatureState,
   (state: IState) => {
