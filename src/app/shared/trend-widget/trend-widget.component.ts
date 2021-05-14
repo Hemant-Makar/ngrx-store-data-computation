60d019bc431ec9e6feeb2f298941a05561d1f279
Hemant-Makar <hemant.makar@gmail.com>
2021-05-14 17:51:36 +0530

    3 files modified, 3 deleted

--- src/app/shared/trend-widget/trend-widget.component.ts
+++ src/app/shared/trend-widget/trend-widget.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IMonitorData } from '../../models/imonitor-data';
+import { StoreDataService } from '../../ngrx-store/store-data.service';
 
 @Component({
   selector: 'app-trend-widget',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trend-widget.component.css']
 })
 export class TrendWidgetComponent implements OnInit {
-
-  constructor() { }
+  public countObservable$: Observable<number>;
+  public dataAObservable$: Observable<IMonitorData[]>;
+  constructor(private storeService: StoreDataService) {}
 
   ngOnInit() {
+    this.countObservable$ = this.storeService.getCount();
+    this.dataAObservable$ = this.storeService.getWidgetCacheData('A');
   }
-
 }
